a32f53a36368a0fa77b25e15fb0da026c55cbf32
philippi.favre <philippi.favre@wedr0307-lx.hevs.ch>
2018-01-09 11:09:00 +0100

    display + main

--- src/Display.java
+++ src/Display.java
@@ -1,4 +1,18 @@
+import hevs.graphics.FunGraphics;
 
 public class Display {
+	final static int GRAPHICS_WIDTH = 64;
+	final static int GRAPHICS_HEIGHT = 64;
+	final static int STARTING_POSITION_X = 32;
+	final static int STARTING_POSITION_Y = 32;
 
+	// Display surface to draw on
+	public static FunGraphics display;
+	
+	// Create and set the windows up
+	public Display(){
+		display = new FunGraphics(GRAPHICS_WIDTH, GRAPHICS_HEIGHT);
+
+	}
 }
+	
--- src/Play.java
+++ src/Play.java
@@ -1,6 +1,7 @@
+import hevs.graphics.FunGraphics;
 
 public class Play {
 	public static void main(String[] args) {
-		
+		new Display();
 	}
 }
