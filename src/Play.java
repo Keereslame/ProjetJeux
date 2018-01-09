bce73a63d2a0f5ea59725a96ee5f2b9cff6d0474
philippi.favre <philippi.favre@wedr0307-lx.hevs.ch>
2018-01-09 10:34:45 +0100

    Mise à jour

--- src/Play.java
+++ src/Play.java
@@ -1,4 +1,6 @@
 
 public class Play {
-
+	public static void main(String[] args) {
+		
+	}
 }
--- src/Snake.java
+++ src/Snake.java
@@ -1,10 +1,5 @@
 
 
 public class Snake {
-//salut
-	//mothode salut
 	
-	public static void main(String[] args){
-		int x = 0;
-	}
 }
