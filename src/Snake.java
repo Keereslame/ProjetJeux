c0aca8c840b1d2564631dea6ff1e11741e261926
mathieu.schopfer <mathieu.schopfer@wedr0305-lx.hevs.ch>
2018-01-09 12:04:15 +0100

    SNAKE

--- src/Snake.java
+++ src/Snake.java
@@ -1,11 +1,49 @@
 
-
 public class Snake {
-	Body body;
-	int[][] position;
+	enum Direction {
+		LEFT, RIGHT, UP, DOWN
+	};
+
+	int length = 3;
+
+	Direction dirSnake = Direction.LEFT;
 	
-	public Snake(){
-		body = new Body(3);
+	int position[][]=new int[64][64];
+	
+	int posHeadX=32;
+	int posHeadY=32;
+	
+	position[posX][posY]=1;
+	
+	for (int i=0;i<length;i++){
+		int posBodyX;
+		int posBodyY;
+		switch (dirSnake){
+		case LEFT:
+			posBodyX=posHeadX+1;
+			posBodyY=posHeadY;
+			
+			break;
+		case RIGHT:
+			posBodyX=posHeadX-1;
+			posBodyY=posHeadY;
+			break;
+		case UP:
+			posBodyX=posHeadX;
+			posBodyY=posHeadY+1;
+			break;
+		case DOWN:
+			posBodyX=posHeadX;
+			posBodyY=posHeadY-1;
+			break;
+		}
+		position[posBodyX][posBodyY]=i+2;
 		
+	}
+
+
+	public Snake() {
+		body = new Body(3);
+
 	}
 }
