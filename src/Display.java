6d0a4677a7336ca4a0f361d26428f291cd57ec76
philippi.favre <philippi.favre@wedr0307-lx.hevs.ch>
2018-01-09 11:14:37 +0100

    display

--- src/Display.java
+++ src/Display.java
@@ -1,18 +1,22 @@
 import hevs.graphics.FunGraphics;
 
 public class Display {
-	final static int GRAPHICS_WIDTH = 64;
-	final static int GRAPHICS_HEIGHT = 64;
-	final static int STARTING_POSITION_X = 32;
-	final static int STARTING_POSITION_Y = 32;
+	//Constants
+	final static int GRAPHICS_WIDTH = 640;
+	final static int GRAPHICS_HEIGHT = 640;
+	final static int STARTING_POSITION_X = 320;
+	final static int STARTING_POSITION_Y = 320;
 
 	// Display surface to draw on
 	public static FunGraphics display;
 	
+	//
+	Snake snake;
+	Food food;
+	
 	// Create and set the windows up
 	public Display(){
 		display = new FunGraphics(GRAPHICS_WIDTH, GRAPHICS_HEIGHT);
-
 	}
 }
 	
