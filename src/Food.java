14892080de591cc869defaea9d7c4faab3ec6c35
philippi.favre <philippi.favre@wedr0307-lx.hevs.ch>
2018-01-09 11:30:01 +0100

    sss

---
+++ src/Food.java
@@ -0,0 +1,28 @@
+import java.awt.Color;
+
+public class Food {
+	int size;
+	Color color;
+	static byte[][] posFood = new byte[64][64];
+	boolean isEating = false;
+
+	static void createFood (boolean a){
+		if (a==false){
+			posFood[(int)Math.random()*64][(int)Math.random()*64];
+			}
+		}
+
+	public Food(int size, Color color) {
+		this.size = size;
+		this.color = color;
+	}
+
+	public void foodEating(boolean b) {
+		isEating = b;
+	}
+
+	public byte[][] getPosFood() {
+		return posFood;
+	}
+
+}
